29a64f7a8d33645d3c497bca353afd649a03306a
JVPFV <99732249+JVPFV@users.noreply.github.com>
2022-08-15 16:26:51 -0300

    Update dolar.php

--- dolar.php
+++ dolar.php
@@ -1,20 +1,20 @@
 <?php
 
 /**
- * Plugin Name:       Plugin de teste
+ * Plugin Name:       Plugin de cotação
  * Plugin URI:        https://example.com/plugins/the-basics/
  * Description:       plugin criado na aula de aw 3min itanhaém.
  * Version:           1.0.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
- * Author:            Edaurdo
+ * Author:            João Victor, Murilo Carreira
  * Author URI:        https://author.example.com/
  *
  */
 
-function teste(){
-	return file_get_contents(plugins_url().'/teste/busca.php');
+function dolar(){
+	return file_get_contents(plugins_url().'/dolar/busca.php');
 }
 
 
-add_shortcode("buscaCep","teste");
+add_shortcode("cotacao","dolar");
