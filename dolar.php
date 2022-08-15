ec5fa894b9874149a962952ba37a05148ae90ff6
JVPFV <99732249+JVPFV@users.noreply.github.com>
2022-08-15 15:40:35 -0300

    Create dolar.php

---
+++ dolar.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Plugin Name:       Plugin de cotação
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       plugin criado na aula de aw 3min itanhaém.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            João Victor
+ * Author URI:        https://author.example.com/
+ *
+ */
+
+function teste(){
+	return file_get_contents(plugins_url().'/dolar/busca.php');
+}
+
+
+add_shortcode("Cotação","aulaaw");
